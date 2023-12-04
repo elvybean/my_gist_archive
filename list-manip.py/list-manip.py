77d811580b09d432675f8a502f46762a8de3fbac
Elvis Obero-Atkins <96076897+elvybean@users.noreply.github.com>
2023-12-04 10:27:14 +0000

    archived list-manip

---
+++ list-manip.py/list-manip.py
@@ -0,0 +1,14 @@
+x = [2, 4, 5, 6, 7, 2, 9] # list
+print(x)
+y = int(input("Enter the number you're looking for: ")) # number searching for
+
+match y in x:
+    case True:
+        z = x.index(y)
+        a = x[x.index(y)]
+    case False:
+        z = len(x)-1
+        a = z[x.index(len(x)-1)]
+
+print(a)
+print(z)
