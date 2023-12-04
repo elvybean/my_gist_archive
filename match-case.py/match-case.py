492f0d99aa7fec79d1ab83156fd770aa607429de
Elvis Obero-Atkins <96076897+elvybean@users.noreply.github.com>
2023-12-04 10:18:03 +0000

    archived match-case

---
+++ match-case.py/match-case.py
@@ -0,0 +1,8 @@
+value = input("Enter a value for 'value': ")
+value2 = input("Enter a value for 'value2': ")
+
+match value > value2:
+    case True:
+        print("'value' is greater than 'value2'")
+    case False:
+        print("'value' is NOT greater than 'value2'")
